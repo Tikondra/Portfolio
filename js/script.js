ac9f9ccccf238c0acd54b6be281acf63c0927e20
Tikondra <tikondra47@gmail.com>
2019-10-15 21:38:05 +0400

    blog

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ function headerMenu() {
     popup.classList.toggle('main-nav_show');
     openPopupButton.classList.toggle('active')
   });
+
+  document.addEventListener('keydown', function (evt) {
+
+    if (evt.keyCode === 27) {
+      popup.classList.remove('main-nav_show');      
+    }
+  });
 };
 
 headerMenu();
