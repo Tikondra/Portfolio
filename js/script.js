bd3b7ea43b8603b05e0ae2a6539cc019b86c418c
Tikondra <tikondra47@gmail.com>
2019-11-04 10:47:50 +0400

    678 & 1024 header, script <678,

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
+if(window.matchMedia('(min-width: 768px)').matches){
+	$(document).ready(function(){
     $(".page-header__btn").click(function(){
         $("#panel").slideToggle("slow");
         $(this).toggleClass("active");
     });
-});
+	});
+}
+
